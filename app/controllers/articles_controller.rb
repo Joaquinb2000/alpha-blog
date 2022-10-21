865269bed4bc9877d18a8be72e92b1d5c6fb5ead
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-10-21 19:00:26 -0400

    Update to rails 6 and adding -Create- and -New- controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,8 +3,17 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-
   def show
     @article = Article.find(params[:id])
   end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
+
+  def new
+  end
+
 end
