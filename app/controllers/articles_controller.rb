89827ba5d997d1e40f237d990486e97297ab4df1
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-10-21 21:23:38 -0400

    Adding edit and update routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,33 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-  def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
-  end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice]= "Successfuly created article."
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice]= "Successfuly edited article."
+      redirect_to @article
+    else
+      render "edit"
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root :to => "pages#home"
   get "about", to: "pages#about"
-  resources :articles, only: [:show, :index, :create, :new]
+  resources :articles
 end
