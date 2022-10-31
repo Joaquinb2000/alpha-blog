2219984f68ca31605e6a23d90185b9410d8327c4
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-10-31 18:16:58 -0400

    Add Signup and user info edit, routes, views, and controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [ :edit, :update ]
+
+  def new
+    @user= User.new
+  end
+
+  def create
+    @user= User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if user.update(user_params)
+      flash[:notice]= "Changes saved succesfully."
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
