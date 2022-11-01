7db79d7752b3a18d7867245dbe8a662cbe1a42db
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-11-01 12:23:40 -0400

    Changing users_controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update ]
+
+  def show
+  end
+
+  def new
+    @user= User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice]= "Account info updated succesfully."
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
+  def create
+    @user= User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username.capitalize}!"
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
