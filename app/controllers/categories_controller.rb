1738fa7d1420281d8be634d94b5c3e105c319497
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-11-05 21:30:50 -0400

    Adding controller and views for update and edit actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
-  before_action :require_admin, only: [ :new, :create ]
+  before_action :require_admin, except: [ :show, :index  ]
+  before_action :set_category, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
@@ -10,7 +11,7 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def create
@@ -23,7 +24,22 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Succesfully updated category details"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   private
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,36 +2,33 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @admin_user = User.create(username: "Joaquin", email: "joaquinberon2000@gmail.com",
-                                                   password: "password", admin: true)
+    @admin_user = User.create(username: "Joaquin", email: "joaquinberon2000@gmail.com", password: "password", admin: true)
     @category = Category.create(name: "Sports")
+    sign_in @admin_user
   end
 
   test "should get index" do
-    get categories_url
+    get categories_path
     assert_response :success
   end
 
   test "should get new" do
-    sign_in @admin_user
     get new_category_url
     assert_response :success
   end
 
   test "should create category" do
-    sign_in @admin_user
     assert_difference('Category.count', 1) do
       post categories_url, params: { category: { name: "Travel" } }
     end
-
     assert_redirected_to category_url(Category.last)
   end
 
   test "should not create category if not admin" do
+    @admin_user.toggle!(:admin)
     assert_no_difference('Category.count') do
       post categories_url, params: { category: { name: "Travel" } }
     end
-
     assert_redirected_to categories_url
   end
 
@@ -40,15 +37,17 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should get edit" do
-  #   get edit_category_url(@category)
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get edit_category_url(@category)
+    assert_response :success
+  end
 
-  # test "should update category" do
-  #   patch category_url(@category), params: { category: {  } }
-  #   assert_redirected_to category_url(@category)
-  # end
+  test "should update category" do
+    patch category_path(@category), params: { category: { name: "Tuti" } }
+    assert_redirected_to category_path(@category)
+    follow_redirect!
+    assert_match "Tuti", response.body
+  end
 
   # test "should destroy category" do
   #   assert_difference('Category.count', -1) do
